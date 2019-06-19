fc82e4c10ea2a4a55e914911825c47ba60d7a5d0
Joanne Hooton <jhooton@EM-02342-MBP.local>
2019-06-19 17:36:25 +0100

    tube svg added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tube from './tube2.svg'
+import { ReactComponent as Logo } from './tube2.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +18,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+        </a> */}
+        <Logo />
       </header>
     </div>
   );
